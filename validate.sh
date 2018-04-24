abdc3860d07d16c8ad3c67ea86ba15f0d0731440
Alexis Reynouard <areynoua@ulb.ac.be>
2018-04-24 11:06:57 +0200

    example files

--- validate.sh
+++ validate.sh
@@ -1,1 +1,3 @@
 xmllint --noout --xinclude --schema bookshop.xsd bookshop.xml |& sed 's+{http://student.ulb.ac.be/~areynoua/infoh509/work1/bookshop/v1/}+{...}+g'
+xmllint --noout --xinclude --schema bookshop.xsd bookshop1.xml |& sed 's+{http://student.ulb.ac.be/~areynoua/infoh509/work1/bookshop/v1/}+{...}+g'
+xmllint --noout --xinclude --schema bookshop.xsd bookshop2.xml |& sed 's+{http://student.ulb.ac.be/~areynoua/infoh509/work1/bookshop/v1/}+{...}+g'
